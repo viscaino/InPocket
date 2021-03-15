af2201925f87acbb3ae84fb664e3b4c4e7103375
viscaino <viscaino@users.noreply.github.com>
2021-03-15 16:21:07 -0300

    150321_1621

--- variables.tf
+++ variables.tf
@@ -55,45 +55,15 @@ variable "public_seclist_name" {
     default = "Pub_SecList"
 }
 
-## Server List Teste
-#
-variable "server_list" {
-    type    = "map"
-    default = {
-        inst0    = {
-            name    = "ServName1",
-            ad      = "hSxN:US-ASHBURN-AD-1",
-            shape   = "VM.Standard.E2.2",
-            image   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda",
-            volname = "ServName1Vol01",
-            volsize = "50",
-            idxctrl = "0",
-            pubip   = "true"
-        }
-
-       inst1    = {
-          name    = "ServName2",
-           ad      = "hSxN:US-ASHBURN-AD-2",
-           shape   = "VM.Standard.E2.2",
-           image   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda",
-           volname = "ServName2Vol01",
-           volsize = "50",
-           idxctrl = "1",
-           pubip   = "true"
-       }
-
-    }
-}
-
 ## SSH Public Key
 #
 variable "ssh_public_key" {
     description = "include public key into userdata directory"
-    default = "userdata/id_rsa.pub"
+    default = "id_rsa.pub"
 }
 
 ## SSH private Key
 #
 variable "ssh_private_key" {
-    default = "userdata/id_rsa"
+    default = "id_rsa"
 }
