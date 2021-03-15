db67bc4edd362a818be6126e4ab3d433372dd4d0
viscaino <viscaino@users.noreply.github.com>
2021-03-15 16:23:13 -0300

    150321_1623

--- compute.tf
+++ compute.tf
@@ -12,21 +12,20 @@
 #--INSTANCE-CREATION------------------------------------------------------------------------------------
 #
 resource "oci_core_instance" "my_pub_instance" {
-    for_each            = "${var.server_list}"
-    availability_domain = "${each.value["ad"]}"
+    availability_domain = "hSxN:US-ASHBURN-AD-2"
     compartment_id      = "${oci_identity_compartment.parent_compartment.id}"
-    display_name        = "${each.value["name"]}"
-    shape               = "${each.value["shape"]}"
+    display_name        = "ServName1"
+    shape               = "VM.Standard.E2.2"
     
     source_details {
         source_type = "image"
-        source_id   = "${each.value["image"]}"
+        source_id   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
     }
 
     create_vnic_details {
         subnet_id           = "${oci_core_subnet.public.id}"
-        display_name        = "${each.value["name"]}_pubvnic"
-        assign_public_ip    = "${each.value["pubip"]}"
+        display_name        = "ServName1_pubvnic"
+        assign_public_ip    = true
     }
     
     metadata    = {
