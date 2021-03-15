4cb0793f0fe64f29a8449ad0bbd938f6cb5b9f25
viscaino <viscaino@users.noreply.github.com>
2021-03-15 14:38:36 -0300

    150321_1438

--- variables.tf
+++ variables.tf
@@ -30,3 +30,89 @@ variable "childmap" {
         Database = "Database Compartment"
     }
 }
+
+## VCN Name:
+#
+variable "vcn_name" {
+    default = "vcn"
+}
+
+## VCN CIDR:
+#
+variable "vcn_cidr" {
+    default = "10.0.0.0/16"
+}
+
+## Public Subnet Name:
+#
+variable "public_subnet_name" {
+    default = "Pub_subnet"
+}
+
+## Public Subnet CIDR:
+#
+variable "public_subnet_cidr" {
+    default = "10.0.2.0/24"
+}
+
+## Public Route Table Name:
+#
+variable "pub_route_tab_name" {
+    default = "Pub_RT"
+}
+
+## Public Security List Name:
+#
+variable "public_seclist_name" {
+    default = "Pub_SecList"
+}
+
+## Security List Rules
+#
+variable "pub_seclist_ingress_rules" {
+    type = "map"
+    default = {
+        role001 = {
+            protocol    = "6",
+            source      = "0.0.0.0/0",
+            min         = "22",
+            max         = "22"
+        }
+        role002 = {
+            protocol    = "6",
+            source      = "0.0.0.0/0",
+            min         = "80",
+            max         = "80"
+        }
+        role003 = {
+            protocol    = "6",
+            source      = "0.0.0.0/0",
+            min         = "1",
+            max         = "65535"
+        }
+    }
+}
+
+variable "pub_seclist_egress_rules" {
+    type = "map"
+    default = {
+        role001 = {
+            protocol    = "6",
+            destination = "0.0.0.0/0",
+            min         = "22",
+            max         = "22"
+        }
+        role002 = {
+            protocol    = "6",
+            destination = "0.0.0.0/0",
+            min         = "80",
+            max         = "80"
+        }
+        role003 = {
+            protocol    = "6",
+            destination = "0.0.0.0/0",
+            min         = "1",
+            max         = "65535"
+        }
+    }
+}
